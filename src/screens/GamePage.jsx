4ac34b72f2a963232d8d33b0f7e8517afd49165e
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2024-08-15 14:55:39 +0200

    started game page

--- src/screens/GamePage.jsx
+++ src/screens/GamePage.jsx
@@ -1,12 +1,77 @@
-import { View, Text } from "react-native"
+import { useContext, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import { Link, useParams } from "react-router-native";
+import { GamesContext } from "../navigation/Index";
+import { COLORS } from "../utils/colors";
+import GamePageBar from "../components/GamePageBar";
 
-const GamePage =() => {
+const GamePage = () => {
+  const [deleting, setDeleting] = useState(false);
 
-    return(
-        <View>
-            <Text>GamePage</Text>
-        </View>
-    )
-}
+  const id = parseInt(useParams().id);
+  const { data, setData } = useContext(GamesContext);
+
+  let game = data.tabletopGames.find((game) => game.id == id);
+  let owner = data.players.find((player) => player.id == game.owner);
+
+  const deleteGame = () => {
+    console.log("Deleting game " + game.name);
+  };
+
+  return (
+    <View style={styles.container}>
+      <GamePageBar deleteGame={deleteGame} />
+      <Image source={{ uri: game.image }} style={styles.gameImage} />
+      <Text style={styles.gameTitle}>{game.name}</Text>
+      <ScrollView
+        contentContainerStyle={{ alignItems: "center" }}
+        style={styles.gameDescriptionContainer}
+      >
+        <Text style={styles.gameDescription}>{game.description}</Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    padding: 10,
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  gameImage: {
+    width: 280,
+    height: 280,
+    margin: 15,
+    borderRadius: 10,
+  },
+  gameTitle: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: COLORS.primary,
+  },
+  gameDescriptionContainer: {
+    maxHeight: 200,
+    marginTop: 20,
+    width: "100%",
+  },
+  gameDescription: {
+    width: "95%",
+    textAlign: "center",
+    paddingHorizontal: 20,
+    paddingHorizontal: 10,
+  },
+});
 
 export default GamePage;
