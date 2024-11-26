d87fd7e4c552f91190d613c4cedf26e20f2f15fb
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2024-11-26 20:16:10 +0100

    fixed starting data

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -4,7 +4,7 @@ const appData = {
       id: 0,
       name: "Game",
       description:
-        "Para añadir un juego, haz click en 'Añadir' en la esquina superior derecha de la pantalla principal. Puedes filtrar los juegos por el número de jugadores y el propietario del juego. Haz click en 'Filtro' para activar el filtro y '╳' para desactivarlo.",
+      "To add a game, press the + button on the right bottom corner of the Games screen. You can filter games by number of playes and game owner.",
       minPlayers: 2,
       maxPlayers: 8,
       duration: 60,
